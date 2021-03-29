3433cecc432443053b6309b2c770067e236bf660
Fulano <detias@outlook.com>
2021-03-28 23:27:46 -0300

    Métodos e propriedades

--- csharppt6-aula1/ByteBank.SistemaAgencia/Program.cs
+++ csharppt6-aula1/ByteBank.SistemaAgencia/Program.cs
@@ -12,18 +12,34 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("teste");
+
             // pagina?argumentos
             // 012345678
 
-            string url = "pagina?argumentos";
-            string argumentos = url.Substring(7);
-            Console.WriteLine(argumentos);
+            //Pegando o Dolar           
+
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumentoDolar = "moedaDestino";
+            string nomeArgumentoReal = "moedaOrigem";
 
 
+            int indiceDolar = palavra.IndexOf(nomeArgumentoDolar);
+            int indiceReal = palavra.IndexOf(nomeArgumentoReal);
+
+            Console.WriteLine($"Indice inicial: {indiceDolar}");
+            Console.WriteLine($"Tamanho da string nome argumento: {nomeArgumentoDolar.Length}");
+            Console.WriteLine($"Nome Argumento: {palavra.Substring(indiceDolar)}");
+            Console.WriteLine($"{palavra.Substring(indiceDolar + nomeArgumentoDolar.Length + 1)}");
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"Indice inicial: {indiceReal}");
+            Console.WriteLine($"Tamanho da string nome argumento: {nomeArgumentoReal.Length}");
+            Console.WriteLine($"Nome Argumento: {palavra.Substring(indiceReal)}");
+            Console.WriteLine($"{palavra.Substring(indiceReal + nomeArgumentoReal.Length + 1)}");
 
             Console.ReadLine();
         }
     }
-
-
 }
---
+++ csharppt6-aula1/ByteBank.SistemaAgencia/Utils/ExtrairValorArgumentosURL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia.Utils
+{
+    public class ExtrairValorArgumentosURL
+    {
+        public string URL { get; }
+        private readonly string _argumentos;
+
+        public ExtrairValorArgumentosURL(string p_url)
+        {
+            if (String.IsNullOrEmpty(p_url))
+                throw new ArgumentException("O argumento URL não pode ser null ou vazio. ", nameof(p_url));
+
+
+            int indiceInterrogacao = p_url.IndexOf('?');
+            _argumentos = URL.Substring(indiceInterrogacao + 1);
+
+            URL = p_url;
+        }
+
+        public string GetValor(string nomeParametro)
+        {
+            return "";
+        }
+
+    }
+}
